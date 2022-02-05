e51c8d7ecc44900735b1df5ff6e6d9f65f220d81
Kaio Cesar <tecnico.kaio@gmail.com>
2022-02-04 23:21:31 -0300

    Red test yet

--- __tests__/test_solucao.py
+++ __tests__/test_solucao.py
@@ -1,4 +1,15 @@
 from src.utils import ArrayToString
+import pytest
+
+
+def test_array_to_string_with_invalid_parameter():
+    """Sending a invalid parameter"""
+    input = None        
+    with pytest.raises(TypeError) as type_error:
+        ArrayToString(input)
+    
+    assert type_error.type is TypeError
+    assert type_error.value.args[0] == 'The function arg should be a list.'
 
 
 def test_array_to_string_success():
@@ -9,5 +20,8 @@ def test_array_to_string_success():
         "state": "Sao Paulo",
         "value": "1234"
         }]
+
     result = ArrayToString(input)
-    assert result == 'Maria Neusa97905796671234'
+
+    assert len(result) == 44
+    assert result == 'Maria Neusa97905796671Sao Paulo  1234       '
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,5 @@
-def ArrayToString(value):
+def ArrayToString(items):
+    """Receive a list and return a concateneted string"""
+    if not isinstance(items, list):
+        raise TypeError('The function arg should be a list.')
     return None
