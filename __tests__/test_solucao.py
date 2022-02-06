5e32ebea51b01ac01c5580c79e43b880077bb9bc
Kaio Cesar <tecnico.kaio@gmail.com>
2022-02-06 00:22:05 -0300

    test case of list to string

--- __tests__/test_solucao.py
+++ __tests__/test_solucao.py
@@ -1,4 +1,4 @@
-from src.utils import ArrayToString
+from src.utils import list_to_string
 import pytest
 
 
@@ -6,14 +6,20 @@ def test_array_to_string_with_invalid_parameter():
     """Sending a invalid parameter"""
     input = None        
     with pytest.raises(TypeError) as type_error:
-        ArrayToString(input)
+        list_to_string(input)
     
     assert type_error.type is TypeError
     assert type_error.value.args[0] == 'The function arg should be a list.'
 
 
-def test_array_to_string_success():
-    """Send a array and return a string"""
+def test_empty_list():
+    """Sending a empty list as parameter"""
+    input = []
+    assert list_to_string(input) == ''
+
+
+def test_list_with_one_dictionary_to_string_success():
+    """Send an list with one dictionary and return a string"""
     input = [{
         "name":"Maria Neusa de Aparecida",
         "cpf": "97905796671",
@@ -21,7 +27,55 @@ def test_array_to_string_success():
         "value": "1234"
         }]
 
-    result = ArrayToString(input)
+    result = list_to_string(input)
 
     assert len(result) == 44
     assert result == 'Maria Neusa97905796671Sao Paulo  1234       '
+
+def test_unordered_list():
+    """send an unordered list and receive the correct string"""
+    input = [{
+        "state": "Sao Paulo",
+        "cpf": "97905796671",
+        "value": "1234",
+        "name":"Maria Neusa de Aparecida",
+        }]
+
+    result = list_to_string(input)
+
+    assert result == 'Maria Neusa97905796671Sao Paulo  1234       '
+
+
+def test_uncompleted_dictionary():
+    """Sending an uncompleted dictionary"""
+    input = [{
+        "name":"Maria Neusa de Aparecida",
+        "cpf": "97905796671",
+        }]
+
+    result = list_to_string(input)
+
+    assert result == ''
+
+
+def test_list_with_two_dictionaries_to_string_success():
+    """Send an list with two dictionaries and return a string"""
+    input = [
+        {
+            "name": "Maria Neusa de Aparecida",
+            "cpf": "97905796671",
+            "state": "Sao Paulo",
+            "value": "1234"
+        },
+        {
+            "name": "Ricardo Fontes",
+            "cpf": "44010762900",
+            "state": "Rio Grande do Sul",
+            "value": "567"
+        }
+    ]
+
+    result = list_to_string(input)
+
+    assert len(result) == 88
+    assert result == 'Maria Neusa97905796671Sao Paulo  1234       Ricardo Fon44010762900Rio Grande 567        '
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,27 @@
-def ArrayToString(items):
+def complete_space(value):
+    space = ''
+    if len(value) < 11:
+        space = " " * (11 - len(value))
+    else:
+        value = value[0:11]
+    return f"{value}{space}"
+
+
+def particular_rule(values):
+    for key in values:
+        values[key] = complete_space(values[key])
+    return f"{values['name']}{values['cpf']}{values['state']}{values['value']}"
+
+
+def list_to_string(items):
     """Receive a list and return a concateneted string"""
+    allowed_keys = ['name', 'cpf', 'state', 'value']
+    final_str = ''
     if not isinstance(items, list):
         raise TypeError('The function arg should be a list.')
-    return None
+
+    for item in items:
+        if len(item.keys()) == 4 and sorted(item.keys()) == sorted(allowed_keys):
+            final_str += particular_rule(item) 
+
+    return final_str
